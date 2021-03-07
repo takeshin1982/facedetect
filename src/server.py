e1c12173d60b71f8ad89b83cd706424d36121d9f
takeshin1982 <80135611+takeshin1982@users.noreply.github.com>
2021-03-07 14:07:42 +0900

    Update server.py

--- src/server.py
+++ src/server.py
@@ -1,5 +1,7 @@
 # coding: utf-8
 from flask import Flask, render_template, request, jsonify
+from importlib import import_module
+import os
 
 app = Flask(__name__)
 
@@ -10,6 +12,10 @@ def root():
                 'result': 'OK'
             }), 200
 
+@app.route('/<userid>', methods=['DETECT'])
+def detect(userid):
+    m = import_module('%s.detect' % userid)
+    return m.execute()
 
 if __name__ == "__main__":
     app.run("0.0.0.0", 5000, threaded=True, debug=False)
